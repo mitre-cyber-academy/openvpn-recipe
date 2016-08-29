e1c27f76ca2b4a2202c39221b1e88b88a919adaf
Robert Clark <rbclark@mitre.org>
2016-08-29 17:02:12 -0400

    Update port that users will access the network on.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,4 @@
 default['openvpn']['private_subnet'] = "10.0.1.0" # /24 internal CIDR block which users are allowed to route to.
+default['openvpn']['private_subnet_cidr'] = "24"
 default['openvpn']['public_ip_or_host'] = "vpn.mitrestemctf.org" # Hostname or IP which clients will access through.
-default['openvpn']['public_port'] = "1194" # Ports which clients will access through.
+default['openvpn']['public_port'] = "11194" # Ports which clients will access through.
