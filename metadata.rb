8ad6693d4d6e944edcfb99bca2b8790357b8fd62
James Tobin <jtobin@mitre.org>
2017-08-15 13:38:20 -0400

    Updated to latest security standards.
    Added TLS support, increased key size, updated encrpytion standard, added message authentication

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'rbclark@mitre.org'
 license          'Apache 2.0'
 description      'Installs/Configures openvpn-recipe'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 depends          'apt'
 depends          'simple_iptables'
 depends          'unattended-upgrades'
